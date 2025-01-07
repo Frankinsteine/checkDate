46ac9ed105016096c8e89d1948382f9ab1d67f4b
Artur Khachatryan <hachatryanarthur@gmail.com>
2025-01-07 21:41:21 +0400

    removed unused functions

--- main.js
+++ main.js
@@ -2,27 +2,12 @@ function isNumeric(value) {
     return /^-?\d+$/.test(value);
 }
 
-function getDaysFromDate(date) {
-    return date.substring(0, 2);
-}
-
-function getMonthsFromDate(date) {
-    return date.substring(3, 5);
-}
-
-function getYearsFromDate(date) {
-    return date.substring(6);
-}
-
 function checkDate() {
     let date = document.getElementById("date").value;
     const wrongDateFormat = "Неверный формат даты!";
     const incorrectDateInterval = "Дата не соответствует промежутку времени";
     const correctDate = "Введена корректная дата!";
     if(date.length != 10 || date[2] != "." || date[5] != ".") {
-        return wrongDateFormat;
-    }
-    if(!isNumeric(getDaysFromDate(date)) || !isNumeric(getMonthsFromDate(date)) || !isNumeric(getYearsFromDate(date))) {
         return wrongDateFormat;
     }
     let dateArray = date.split(".");
